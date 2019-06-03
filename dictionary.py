858aaf506d0daaf3db85d015d06bad383ad2130c
Rahul Kashyap <rahul.kashyap.17001@iitgoa.ac.in>
2019-06-03 21:21:56 +0530

    Different Cases allowed

--- dictionary.py
+++ dictionary.py
@@ -2,11 +2,15 @@ import json
 
 dictionary = json.load(open("./data.json"))
 
+
 def meaning(word):
     if word in dictionary:
         return dictionary[word]
     else:
         return "Word not found. Please check the word"
 
+
 word = input("Please enter the word: ")
+word = word.lower()
+word = word.replace(" ", "")
 print(meaning(word))
